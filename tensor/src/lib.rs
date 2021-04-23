fcc078089ec9270c5debd0c3302a270b5c0f99f6
ViliamVadocz <viliam.vadocz@gmail.com>
2021-04-23 20:49:05 +0200

    Expose structs and traits in lib

--- tensor/src/lib.rs
+++ tensor/src/lib.rs
@@ -1,4 +1,11 @@
 #![feature(const_generics, const_evaluatable_checked)]
 #![allow(incomplete_features)]
 
-pub mod tensor;
+mod tensor;
+
+pub use tensor::{Tensor, Tensor1, Tensor2, Tensor3, reshape};
+pub use tensor::elementwise::ElementWiseTensor;
+#[cfg(feature = "rand")]
+pub use rand::distributions as rand_distr;
+#[cfg(feature = "rand")]
+pub use tensor::random::RandomTensor;
