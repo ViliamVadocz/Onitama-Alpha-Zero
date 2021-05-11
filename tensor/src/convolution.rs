09098f6407bcaee81f51df4c1453ed62d217d294
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-05-11 19:53:26 +0200

    Add convolution for specific output size

--- tensor/src/convolution.rs
+++ tensor/src/convolution.rs
@@ -18,7 +18,7 @@ where
         res
     }
 
-    /// Naive convolution with default padding to preserve shape.
+    /// Naive convolution with default padding to **preserve** shape.
     pub fn convolve_with_pad<const KERN_L: usize>(&self, kernel: &Tensor1<T, KERN_L>) -> Tensor1<T, L>
     where
         T: AddAssign + MulAssign,
@@ -27,6 +27,23 @@ where
         for (i, &val) in kernel.0.iter().enumerate() {
             let i = i as isize;
             res += &self.slice_with_pad(i - KERN_L as isize / 2).scale(val)
+        }
+        res
+    }
+
+    /// Naive convolution with default padding to **change** shape.
+    pub fn convolve_with_pad_to<const KERN_L: usize, const OUT_L: usize>(
+        &self,
+        kernel: &Tensor1<T, KERN_L>,
+    ) -> Tensor1<T, OUT_L>
+    where
+        T: AddAssign + MulAssign,
+    {
+        let pad = (OUT_L + KERN_L - L - 1) as isize;
+        let mut res = Tensor1::default();
+        for (i, &val) in kernel.0.iter().enumerate() {
+            let i = i as isize;
+            res += &self.slice_with_pad(i - pad / 2).scale(val)
         }
         res
     }
@@ -56,7 +73,7 @@ where
         res
     }
 
-    /// Naive convolution with default padding to preserve shape.
+    /// Naive convolution with default padding to **preserve** shape.
     pub fn convolve_with_pad<const KERN_C: usize, const KERN_R: usize>(
         &self,
         kernel: &Tensor2<T, KERN_C, KERN_R>,
@@ -64,15 +81,43 @@ where
     where
         T: AddAssign + MulAssign,
         [(); KERN_C * KERN_R]: ,
-        [(); (C + KERN_C - 1) * (R + KERN_R - 1)]: ,
     {
         let mut res = Tensor2::default();
         let kern_r = KERN_R as isize;
         let kern_c = KERN_C as isize;
         for (i, &val) in kernel.0.iter().enumerate() {
             let i = i as isize;
-            let c = i % kern_c as isize - kern_c / 2;
-            let r = i / kern_c as isize - kern_r / 2;
+            let c = i % kern_c - kern_c / 2;
+            let r = i / kern_c - kern_r / 2;
+            res += &self.slice_with_pad(c, r).scale(val);
+        }
+        res
+    }
+
+    /// Naive convolution with default padding to **change** shape.
+    pub fn convolve_with_pad_to<
+        const KERN_C: usize,
+        const KERN_R: usize,
+        const OUT_C: usize,
+        const OUT_R: usize,
+    >(
+        &self,
+        kernel: &Tensor2<T, KERN_C, KERN_R>,
+    ) -> Tensor2<T, OUT_C, OUT_R>
+    where
+        T: AddAssign + MulAssign,
+        [(); KERN_C * KERN_R]: ,
+        [(); OUT_C * OUT_R]: ,
+    {
+        let pad_c = (OUT_C + KERN_C - C - 1) as isize;
+        let pad_r = (OUT_R + KERN_R - R - 1) as isize;
+        let mut res = Tensor2::default();
+        let _kern_r = KERN_R as isize;
+        let kern_c = KERN_C as isize;
+        for (i, &val) in kernel.0.iter().enumerate() {
+            let i = i as isize;
+            let c = i % kern_c - pad_c / 2;
+            let r = i / kern_c - pad_r / 2;
             res += &self.slice_with_pad(c, r).scale(val);
         }
         res
@@ -104,7 +149,7 @@ where
         res
     }
 
-    /// Naive convolution with default padding to preserve shape.
+    /// Naive convolution with default padding to **preserve** shape.
     pub fn convolve_with_pad<const KERN_D1: usize, const KERN_D2: usize, const KERN_D3: usize>(
         &self,
         kernel: &Tensor3<T, KERN_D1, KERN_D2, KERN_D3>,
@@ -126,6 +171,40 @@ where
         }
         res
     }
+
+    /// Naive convolution with default padding to **change** shape.
+    pub fn convolve_with_pad_to<
+        const KERN_D1: usize,
+        const KERN_D2: usize,
+        const KERN_D3: usize,
+        const OUT_D1: usize,
+        const OUT_D2: usize,
+        const OUT_D3: usize,
+    >(
+        &self,
+        kernel: &Tensor3<T, KERN_D1, KERN_D2, KERN_D3>,
+    ) -> Tensor3<T, OUT_D1, OUT_D2, OUT_D3>
+    where
+        T: AddAssign + MulAssign,
+        [(); KERN_D1 * KERN_D2 * KERN_D3]: ,
+        [(); OUT_D1 * OUT_D2 * OUT_D3]: ,
+    {
+        let pad_d1 = (OUT_D1 + KERN_D1 - D1 - 1) as isize;
+        let pad_d2 = (OUT_D2 + KERN_D2 - D2 - 1) as isize;
+        let pad_d3 = (OUT_D3 + KERN_D3 - D3 - 1) as isize;
+        let mut res = Tensor3::default();
+        let kern_d1 = KERN_D1 as isize;
+        let kern_d2 = KERN_D2 as isize;
+        let _kern_d3 = KERN_D3 as isize;
+        for (i, &val) in kernel.0.iter().enumerate() {
+            let i = i as isize;
+            let d1 = i % kern_d1 - pad_d1 / 2;
+            let d2 = (i / kern_d1) % kern_d2 - pad_d2 / 2;
+            let d3 = i / (kern_d1 * kern_d2) - pad_d3 / 2;
+            res += &self.slice_with_pad(d1, d2, d3).scale(val);
+        }
+        res
+    }
 }
 
 #[cfg(test)]
@@ -141,6 +220,10 @@ mod tests {
             a.convolve_with_pad(&kernel),
             Vector::new([8, 14, 20, 26, 32, 38, 20])
         );
+        assert_eq!(
+            a.convolve_with_pad_to(&kernel),
+            Vector::new([3, 8, 14, 20, 26, 32, 38, 20, 7])
+        );
 
         let b = Vector::new([1, 2, 3, 4, 5, 6]);
         let kernel = Vector::new([1, 2, 3, 4]);
@@ -149,6 +232,7 @@ mod tests {
             b.convolve_with_pad(&kernel),
             Vector::new([11, 20, 30, 40, 50, 32])
         );
+        assert_eq!(b.convolve_with_pad_to(&kernel), Vector::new([20, 30, 40, 50, 32]))
     }
 
     #[test]
@@ -168,6 +252,10 @@ mod tests {
         assert_eq!(
             a.convolve_with_pad(&kernel),
             Matrix::new([4, 11, 18, 18, 37, 47, 36, 67, 77])
+        );
+        assert_eq!(
+            a.convolve_with_pad_to(&kernel),
+            Matrix::<_, 4, 2>::new([18, 37, 47, 21, 36, 67, 77, 33])
         );
 
         #[rustfmt::skip]
@@ -191,6 +279,10 @@ mod tests {
             96, 183, 204,
             54, 97, 107,
         ]));
+        assert_eq!(
+            b.convolve_with_pad_to(&kernel),
+            Matrix::<_, 3, 2>::new([120, 141, 66, 183, 204, 93])
+        );
     }
 
     #[test]
@@ -220,6 +312,10 @@ mod tests {
             60, 124, 146,
             140, 278, 314
         ]));
+        assert_eq!(
+            a.convolve_with_pad_to(&kernel),
+            Tensor3::<_, 2, 1, 2>::new([278, 314, 97, 107])
+        )
     }
 }
 
